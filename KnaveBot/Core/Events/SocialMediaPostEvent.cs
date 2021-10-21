1be44979984d3f8524e96bdd8c8363dd33b64c92
Skyhighjinks <skyhighjinksdev123@gmail.com>
2021-10-21 15:08:06 +0100

    Added a bunch of concept ideas to look into

---
+++ KnaveBot/Core/Events/SocialMediaPostEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnaveBot.Core.Events
+{
+  public class SocialMediaPostEvent
+  {
+    // Notify people when Twitch streamer goes live, youtube is posted etc...
+  }
+}
---
+++ KnaveBot/Core/Moderation/ChatModeration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnaveBot.Core.Moderation
+{
+  public class ChatModeration
+  {
+    // Filters out certain keywords - can be disabled, the list can also be added to or removed for the specific server.
+  }
+}
